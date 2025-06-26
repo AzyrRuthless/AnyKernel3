36b5a48cfe2e9d5ad3f030f5c41f537334fd235c
Azyr Ruthless <132790292+AzyrRuthless@users.noreply.github.com>
2025-06-26 21:19:55 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=curtana
 device.name3=excalibur
 device.name4=gram
 device.name5=joyeuse
-supported.versions=11 - 14
+supported.versions=11 - 13
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
